c1f592210aee999f8b82231d8f73a2f386bb1aff
Anton Novozhilov <novozhilovantonio@yandex.ru>
2025-06-10 21:03:25 +0300

    Первая иттерация

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector('.places__list');
+const template = document.querySelector('#card-template').content;
+
+function createCard (cardData, cardDelete) {
+    const cardElement = template.querySelector('.card').cloneNode(true);
+    const image = cardElement.querySelector('.card__image');
+    const delButton = cardElement.querySelector('.card__delete-button');
+    const title = cardElement.querySelector('.card__title');
+    const likeButton = cardElement.querySelector('.card__like-button');
+
+    image.src = cardData.link;
+    /*image.alt = cardData.title*/
+    title.textContent = cardData.name;
+
+    delButton.addEventListener ('click', () => {
+        cardDelete(cardElement)
+    });
+
+    return cardElement;
+};
+
+initialCards.forEach(cardData => {
+    const initialCard = createCard(cardData, handleDelete);
+    cardList.appendChild(initialCard);
+});
+
+function handleDelete (cardElement) {
+    cardElement.remove()
+};
+

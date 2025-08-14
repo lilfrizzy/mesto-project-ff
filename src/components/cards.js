52ad59f3f020adc2c4b18a9e30537a8e9e6bbc02
Anton Novozhilov <novozhilovantonio@yandex.ru>
2025-08-14 20:28:27 +0300

    Восстановленый проект

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,33 +1,8 @@
 //import { escape } from "core-js/fn/regexp";
 
-export const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
-];
+import { likeCard, unlikeCard } from './api.js';
 
-export function createCard(cardData, handleDeleteCard, handleOpenImage) {
+export function createCard(cardData, handleDeleteCard, handleOpenImage, userId) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     
@@ -35,18 +10,37 @@ export function createCard(cardData, handleDeleteCard, handleOpenImage) {
     const cardTitle = cardElement.querySelector('.card__title');
     const deleteButton = cardElement.querySelector('.card__delete-button');
     const likeButton = cardElement.querySelector('.card__like-button');
+    const likeCount = cardElement.querySelector('.card__like-count');
+    const cardId = cardData._id;
 
     cardImage.src = cardData.link;
     cardImage.alt = cardData.name;
     cardTitle.textContent = cardData.name;
+    likeCount.textContent = cardData.likes.length;
+    
+    if (cardData.owner._id == userId) {
+      deleteButton.addEventListener('click', () => handleDeleteCard(cardId, cardElement));
+    } else {
+      deleteButton.remove();
+    }
 
-    deleteButton.addEventListener('click', () => handleDeleteCard(cardElement));
+    if (cardData.likes.some(like => like._id === userId)) {
+        likeButton.classList.add('card__like-button_is-active');
+    }
+
     cardImage.addEventListener('click', () => handleOpenImage(cardData.name, cardData.link));
-    likeButton.addEventListener('click', () => likeButton.classList.toggle('card__like-button_is-active'));
+
+    likeButton.addEventListener('click', () => {
+      const isLiked = likeButton.classList.contains('card__like-button_is-active');
+      const action = isLiked ? unlikeCard : likeCard;
+      action(cardId)
+        .then(updatedCard => {
+          // Обновляем количество лайков с сервера
+          likeCount.textContent = updatedCard.likes.length;
+          likeButton.classList.toggle('card__like-button_is-active');
+        })
+        .catch(err => console.error(err));
+  });
 
     return cardElement;
-};
-
-export function handleDeleteCard(cardElement) {
-    cardElement.remove();
 };
